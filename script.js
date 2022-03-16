d52d596accae0c8b61ba338b374c9807afe8ed4b
Mukund Aggarwal <le.rouge.lion@gmail.com>
2022-03-16 10:45:49 +0530

    Worlde updated

--- script.js
+++ script.js
@@ -71,21 +71,21 @@ const getCountryValues = async () => {
             label: 'Reported active COVID Cases - ' + countryFullNm,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.7)',
-                'rgba(255, 159, 64, 0.7)',
+                /*'rgba(255, 159, 64, 0.7)',
                 'rgba(255, 205, 86, 0.7)',
                 'rgba(75, 192, 192, 0.7)',
                 'rgba(54, 162, 235, 0.7)',
                 'rgba(153, 102, 255, 0.7)',
-                'rgba(201, 203, 207, 0.7)'
+                'rgba(201, 203, 207, 0.7)'*/
             ],
             borderColor: [
                 'rgb(255, 99, 132)',
-                'rgb(255, 159, 64)',
+                /*'rgb(255, 159, 64)',
                 'rgb(255, 205, 86)',
                 'rgb(75, 192, 192)',
                 'rgb(54, 162, 235)',
                 'rgb(153, 102, 255)',
-                'rgb(201, 203, 207)'
+                'rgb(201, 203, 207)'*/
             ],
             borderWidth: 1,
             data: last30DaysArr,
